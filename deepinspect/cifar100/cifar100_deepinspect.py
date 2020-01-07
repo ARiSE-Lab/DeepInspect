375e781cfd0745a36df60b8c1e09aa806d7d0452
Yuchi Tian <yt2667@columbia.edu>
2020-01-06 23:53:02 -0500

    update cifar100

--- deepinspect/cifar100/cifar100_deepinspect.py
+++ deepinspect/cifar100/cifar100_deepinspect.py
@@ -1,20 +1,11 @@
-import math, os, random, json, pickle, sys, pdb, csv, copy
-import string, shutil, time, argparse
+import math, os, random, pickle, sys, csv, copy
 import numpy as np
-
-#from sklearn.metrics import average_precision_score
-#from sklearn.metrics import roc_auc_score
 from tqdm import tqdm as tqdm
 
 import torch.nn.functional as F
 import torch, torchvision
 import torch.nn as nn
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-import torch.optim as optim
-import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
-from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 import dataset
 import model
@@ -22,11 +13,11 @@ globalcoverage = []  # [{file, label, layercoverage, yhat}]
 globalprofile = []
 globallayercount = 0
 currentlabels = []
-layernumber = 7
-for i in xrange(layernumber):
+convlayernumber = 7
+for i in xrange(convlayernumber):
     globalprofile.append([])
 
-gthreshold = 0.9
+gthreshold = 0.5 # neuron coverage threshold
 def hook_all_conv_layer(net, handler):
     for l in net._modules.keys():
         if isinstance(net._modules.get(l), torch.nn.modules.conv.Conv2d):
@@ -48,7 +39,7 @@ def get_channel_coverage_group_exp(self, input, output):
         if "layercoverage" not in globalcoverage[d]:
             globalcoverage[d]["layercoverage"] = []
         # total 7 cnn layer
-        assert len(globalcoverage[d]["layercoverage"]) <= 7
+        assert len(globalcoverage[d]["layercoverage"]) <= convlayernumber
 
         # print('covered percentage: ' + str(len(covered_channel)*1.0/len(output.data[0])))
 
@@ -58,22 +49,20 @@ def get_channel_coverage_group_exp(self, input, output):
         # print('covered percentage: ' + str(len(covered_channel)*1.0/len(output.data[0])))
         globalcoverage[d]["layercoverage"].append((len(output.data[0]), covered_channel))
 
-    if len(globalcoverage[-1]["layercoverage"]) == 7:
-        # with open('globalcoveragecocoexp_val.pickle', 'ab') as handle:
+    if len(globalcoverage[-1]["layercoverage"]) == convlayernumber:
         with open('globalcoveragecifar100_test_'+ str(gthreshold) + '.pickle', 'ab') as handle:
             pickle.dump(globalcoverage, handle, protocol=pickle.HIGHEST_PROTOCOL)
         globalcoverage = []
 
+# collecting neuron coverage for all testing data
 def get_test_coverage():
     torch.manual_seed(1)
     torch.cuda.manual_seed(1)
 
     # data loader and model
-
     train_loader, test_loader = dataset.get100(batch_size=200, num_workers=1)
-
     cifar100_model = model.cifar100(128, pretrained=None)
-    checkpoint = torch.load(os.path.join('./best.pth'))
+    checkpoint = torch.load(os.path.join('./latest.pth'))
     cifar100_model.load_state_dict(checkpoint)
     cifar100_model.cuda()
     hook_all_conv_layer(cifar100_model, get_channel_coverage_group_exp)
@@ -84,7 +73,10 @@ def get_test_coverage():
     count = 0
     yhats = []
     labels = []
-    for data, target in test_loader:
+    print("collecting neuron coverage for all testing data...")
+
+    t = tqdm(test_loader, desc="Evaluating on Test:")
+    for batch_idx, (data, target) in enumerate(t):
         labels = labels + list(target)
         indx_target = target.clone()
         data, target = data.cuda(), target.cuda()
@@ -114,7 +106,7 @@ def get_test_coverage():
 def deepinspect(sample_10):
     #compute neuron-feature score
     #feature->neurons mapping
-    total_layers = 7
+    total_layers = convlayernumber
     with open('globalcifar100_test_yhats.pickle', 'rb') as handle:
         test_yhats = pickle.load(handle)
     with open('globalcifar100_test_labels.pickle', 'rb') as handle:
@@ -127,6 +119,7 @@ def deepinspect(sample_10):
     #compute features_sum    
     featuresmap = {}
 
+    print("compute probability matrix... ")
     infile = open('globalcoveragecifar100_test_'+ str(gthreshold) + '.pickle', 'r')
     processed = 0
     while 1:
@@ -134,7 +127,7 @@ def deepinspect(sample_10):
             globalcoverage = pickle.load(infile)
             assert len(globalcoverage) <= 200
             for g in globalcoverage:
-                assert len(g["layercoverage"]) == 7
+                assert len(g["layercoverage"]) == convlayernumber
                 if processed in sample_10:
                     processed = processed + 1
                     continue
@@ -205,11 +198,8 @@ def deepinspect(sample_10):
 
 
     labels_list = list(xrange(100))
-    
-
     #compute pairwise distance
-    #distance1 based on probability score
-    #distance2 based on sensitivity score
+    #distance1 based on probability matrix
     distance1 = {}
     for l1 in labels_list:
         for l2 in labels_list:
@@ -244,7 +234,7 @@ def deepinspect(sample_10):
             writer.writerow(csvrecord)
 
 def get_90_csv_files(sample_10):
-    total_layers = 7
+    total_layers = convlayernumber
     with open('globalcifar100_test_yhats.pickle', 'rb') as handle:
         yhats_raw = pickle.load(handle)
     with open('globalcifar100_test_labels.pickle', 'rb') as handle:
@@ -321,9 +311,9 @@ def get_90_csv_files(sample_10):
             writer.writerow(csvrecord)
 
 if __name__ == '__main__':
-    get_test_coverage()
+    get_test_coverage() # collecting neuron coverage for all testing data
     np.random.seed(0)
-    sample_10 = np.random.choice(10000, 1000, replace=False)
-    deepinspect(sample_10=sample_10)
-    get_90_csv_files(sample_10=sample_10)
+    sample_10 = np.random.choice(10000, 1000, replace=False) # keep randomly sampled 10% for other use
+    deepinspect(sample_10=sample_10) # inspect the model using the model's prediction on 90% of testing data
+    get_90_csv_files(sample_10=sample_10) # generate necessary information for 90% of testing data
     
